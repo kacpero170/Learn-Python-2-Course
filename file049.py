01956b689bec02691e712e3e0cbd83cc049d9352
Kacper Staroń <164014637+kacpero170@users.noreply.github.com>
2024-04-28 01:26:00 +0200

    Add files via upload

---
+++ file049.py
@@ -0,0 +1,79 @@
+grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+print ("Grades:", grades)
+
+#NEXT
+
+grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+
+def print_grades(grades_input):
+  i = 0
+  while i in range(len(grades_input)):
+    print (grades[i])
+    i += 1
+print(print_grades(grades))
+
+#NEXT
+
+grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+
+def grades_sum(scores):
+  total = 0
+  for score in scores: 
+    total += score
+  return total
+print(grades_sum(grades))
+
+#NEXT
+
+grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+
+def grades_sum(scores):
+  total = 0
+  for score in scores: 
+    total += score
+  return total
+
+print (grades_sum(grades))
+
+def grades_average(grades_input):
+  sum_of_grades = grades_sum(grades_input)
+  average = sum_of_grades / float(len(grades_input))
+  return average
+
+print (grades_average(grades))
+
+#NEXT
+
+grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+
+def print_grades(grades_input):
+  for grade in grades_input:
+    print (grade)
+
+def grades_sum(scores):
+  total = 0
+  for score in scores: 
+    total += score
+  return total
+    
+def grades_average(grades_input):
+  sum_of_grades = grades_sum(grades_input)
+  average = sum_of_grades / float(len(grades_input))
+  return average
+
+def grades_variance(grades):
+    variance = 0
+    for number in grades:
+        variance += (grades_average(grades) - number) ** 2
+    return variance / len(grades)
+
+def grades_std_deviation(variance):
+  return variance ** 0.5
+
+variance = grades_variance(grades)
+
+print(print_grades(grades))
+print(grades_sum(grades))
+print(grades_average(grades))
+print(grades_variance(grades))
+print(grades_std_deviation(variance))
